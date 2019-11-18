21bc2785ac308ad859c748aba15f9109eaf30121
EricLi404 <leiflyy@outlook.com>
2019-11-19 02:36:40 +0800

    1260 update error

--- leetcode/163/1260.py
+++ leetcode/163/1260.py
@@ -7,17 +7,17 @@ class Solution:
         one_line_grid = []
         for a_l in grid:
             one_line_grid += a_l
-        o = [[]]
+        o = grid
         for i in range(len(one_line_grid)):
-            #  o.append(one_line_grid[(i + real_k) % len(one_line_grid)])
+            o[int(i / len(grid[0]))][i % len(grid[0])] = one_line_grid[(i-real_k)%(len((grid)*len(grid[0])))]
         return o
 
 
 c = Solution()
 
-grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+grid = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
 #  grid = [[1]]
-k = 10
+k = 2
 
 o = c.shiftGrid(grid, k)
 print(o)
