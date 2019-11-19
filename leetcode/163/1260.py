d8e954ba071e4e2e73c09c7ca693b9a26811f0d0
EricLi404 <leiflyy@outlook.com>
2019-11-19 09:51:58 +0800

    1260 67% 100%

--- leetcode/163/1260.py
+++ leetcode/163/1260.py
@@ -3,14 +3,18 @@
 class Solution:
     # def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
     def shiftGrid(self, grid, k):
-        real_k = k % (len(grid) + len(grid[0]))
+        a = len(grid)
+        b = len(grid[0])
+        c = a * b
+        real_k = k % c
+        if real_k == 0:
+            return grid
         one_line_grid = []
         for a_l in grid:
             one_line_grid += a_l
-        o = grid
-        for i in range(len(one_line_grid)):
-            o[int(i / len(grid[0]))][i % len(grid[0])] = one_line_grid[(i-real_k)%(len((grid)*len(grid[0])))]
-        return o
+        for i in range(c):
+            grid[int(i / b)][i % b] = one_line_grid[(i - real_k) % c]
+        return grid
 
 
 c = Solution()
