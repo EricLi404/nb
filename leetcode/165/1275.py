385a2f29d923106fc5c3ef5b641e7e95d1f456a4
EricLi404 <leiflyy@outlook.com>
2019-12-03 21:13:28 +0800

    xxoo棋

---
+++ leetcode/165/1275.py
@@ -0,0 +1,19 @@
+# class Solution:
+#     def tictactoe(self, moves: List[List[int]]) -> str:
+
+
+class Solution:
+    def tictactoe(self, moves):
+        r = "Pending"
+
+
+
+        return r
+
+
+moves = [[0, 0], [2, 0], [1, 1], [2, 1], [2, 2]]
+
+a = Solution()
+c = a.tictactoe(moves)
+
+print(c)
