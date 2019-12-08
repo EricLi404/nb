4f78808eb1d4afd4e76d0eb13871e90bafef95b2
EricLi404 <leiflyy@outlook.com>
2019-12-08 19:17:27 +0800

    井字棋 1275

--- leetcode/165/1275.py
+++ leetcode/165/1275.py
@@ -1,19 +1,69 @@
-# class Solution:
-#     def tictactoe(self, moves: List[List[int]]) -> str:
+# A 和 B 在一个 3 x 3 的网格上玩井字棋。
+#
+# 井字棋游戏的规则如下：
+#
+# 玩家轮流将棋子放在空方格 (" ") 上。
+# 第一个玩家 A 总是用 "X" 作为棋子，而第二个玩家 B 总是用 "O" 作为棋子。
+# "X" 和 "O" 只能放在空方格中，而不能放在已经被占用的方格上。
+# 只要有 3 个相同的（非空）棋子排成一条直线（行、列、对角线）时，游戏结束。
+# 如果所有方块都放满棋子（不为空），游戏也会结束。
+# 游戏结束后，棋子无法再进行任何移动。
+# 给你一个数组 moves，其中每个元素是大小为 2 的另一个数组（元素分别对应网格的行和列），它按照 A 和 B 的行动顺序（先 A 后 B）记录了两人各自的棋子位置。
+#
+# 如果游戏存在获胜者（A 或 B），就返回该游戏的获胜者；如果游戏以平局结束，则返回 "Draw"；如果仍会有行动（游戏未结束），则返回 "Pending"。
+#
+# 你可以假设 moves 都 有效（遵循井字棋规则），网格最初是空的，A 将先行动。
 
 
 class Solution:
     def tictactoe(self, moves):
+# class Solution:
+#     def tictactoe(self, moves: List[List[int]]) -> str:
         r = "Pending"
+        x = []
+        o = []
+        for i in range(len(moves)):
+            if i % 2 == 0:
+                x.append(moves[i])
+            else:
+                o.append(moves[i])
+            if len(x) >= 3:
+                if self.judge(x):
+                    return "A"
+                elif self.judge(o):
+                    return "B"
+        if len(moves) == 9:
+            return "Draw"
+        else:
+            return "Pending"
+
+    def judge(self, m):
+        s = [
+            [[0, 0], [0, 1], [0, 2]],
+            [[1, 0], [1, 1], [1, 2]],
+            [[2, 0], [2, 1], [2, 2]],
+            [[0, 0], [1, 0], [2, 0]],
+            [[0, 1], [1, 1], [2, 1]],
+            [[0, 2], [1, 2], [2, 2]],
+            [[0, 0], [1, 1], [2, 2]],
+            [[0, 2], [1, 1], [2, 0]],
+        ]
+        flags = 0
+        if len(m) >= 3:
+            for i in s:
+                flags = 0
+                for j in m:
+                    if j in i:
+                        flags += 1
+                if flags == 3:
+                    return True
+        return False
 
 
+if __name__ == '__main__':
+    moves = [[0, 0], [2, 0], [1, 1], [2, 1], [2, 2]]
 
-        return r
+    a = Solution()
+    c = a.tictactoe(moves)
 
-
-moves = [[0, 0], [2, 0], [1, 1], [2, 1], [2, 2]]
-
-a = Solution()
-c = a.tictactoe(moves)
-
-print(c)
+    print(c)
