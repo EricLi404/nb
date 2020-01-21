e5724b0684139dd5fca9336c8c17d9e8a115c042
EricLi404 <leiflyy@outlook.com>
2020-01-21 10:43:50 +0800

    4

---
+++ leetcode/sequence/4.median-of-two-sorted-arrays.py
@@ -0,0 +1,18 @@
+class Solution:
+    # def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    def findMedianSortedArrays(self, nums1, nums2) -> float:
+        n = nums1 + nums2
+        n.sort()
+        l = len(n)
+        if l % 2 == 1:
+            return n[l // 2]
+        else:
+            return (n[l // 2] + n[l // 2 - 1]) / 2
+
+
+if __name__ == '__main__':
+    ss = Solution()
+    n1 = [1, 3]
+    n2 = [2, 4]
+    cc = ss.findMedianSortedArrays(n1, n2)
+    print(cc)
