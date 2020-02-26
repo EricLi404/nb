649a0adf152bbb3a8a72158ba911bce9d4507406
EricLi404 <leiflyy@outlook.com>
2020-02-26 10:29:46 +0800

    9

---
+++ leetcode/tencent-hot50/9_palindrome-number.py
@@ -0,0 +1,5 @@
+# https://leetcode-cn.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return True if x >= 0 and x == int(str(x)[::-1]) else False
