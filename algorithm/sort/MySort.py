f1a5fb3dd8e62606e41f76cb6605f66eb89282e1
EricLi404 <leiflyy@outlook.com>
2020-02-28 16:16:44 +0800

    sort

--- algorithm/sort/MySort.py
+++ algorithm/sort/MySort.py
@@ -17,7 +17,26 @@ def selection_sort(nums):
     return nums
 
 
+def BubbleSort(lst):
+    n = len(lst)
+    if n <= 1:
+        return lst
+    for i in range(0, n):
+        for j in range(0, n - i - 1):
+            if lst[j] > lst[j + 1]:
+                (lst[j], lst[j + 1]) = (lst[j + 1], lst[j])
+
+
+def quickSort(nums):
+    if len(nums) <= 1:
+        return nums
+    pivot = nums[0]
+    left = [nums[i] for i in range(1, len(nums)) if nums[i] < pivot]
+    right = [nums[i] for i in range(1, len(nums)) if nums[i] >= pivot]
+    return quickSort(left) + [pivot] + quickSort(right)
+
+
 if __name__ == '__main__':
     ll = [3, 2, 4, 7, 1]
     # print(bubble_sort(ll))
-    print(selection_sort(ll))
+    print(quickSort(ll))
