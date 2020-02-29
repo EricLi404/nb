d6188e8f9d8c30b734584c271893728c0bc7731e
EricLi404 <leiflyy@outlook.com>
2020-02-29 21:21:19 +0800

    11 better

--- leetcode/tencent-hot50/11_container-with-most-water.py
+++ leetcode/tencent-hot50/11_container-with-most-water.py
@@ -8,26 +8,15 @@ class Solution:
         j = len(height) - 1
         s = 0
         while i < j:
-            ns = (j - i) * min(height[i], height[j])
-            s = ns if ns > s else s
             if height[i] <= height[j]:
+                s = max(s, height[i] * (j - i))
                 i += 1
             else:
+                s = max(s, height[j] * (j - i))
                 j -= 1
         return s
 
 
-class Solution2:
-    def maxArea(self, height: List[int]) -> int:
-        i, j, res = 0, len(height) - 1, 0
-        while i < j:
-            if height[i] < height[j]:
-                res = max(res, height[i] * (j - i))
-                i += 1
-            else:
-                res = max(res, height[j] * (j - i))
-                j -= 1
-        return res
 
 
 if __name__ == '__main__':
