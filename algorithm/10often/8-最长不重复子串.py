e5f87e7df4219e75e7e160b95a8613a94b4442ea
EricLi404 <leiflyy@outlook.com>
2020-03-01 17:00:06 +0800

    cm

--- algorithm/10often/8-最长不重复子串.py
+++ algorithm/10often/8-最长不重复子串.py
@@ -2,6 +2,8 @@
 
 # 给定一个字符串，找出没有重复字符的最长的子串。例如输入“abcbefgf”，答案是 “cbefg”
 
+
+# 边界问题！！！
 def f(s):
     i, j = 0, 0
     l = list(s)
@@ -20,6 +22,20 @@ def f(s):
     return "".join(m)
 
 
+def f1(s):
+    d = {}
+    i, j = 0, 0
+    sl = []
+    while j < len(s):
+        if s[j] in d:
+            i = max(d[s[j]] + 1, i)
+        d[s[j]] = j
+        if j - i + 1 > len(sl):
+            sl = s[i:j + 1]
+        j += 1
+    return sl
+
+
 def f2(s):
     m, i, j = 0, 0, 0
     l = list(s)
@@ -36,6 +52,6 @@ def f2(s):
 
 
 if __name__ == '__main__':
-    # c = f("abcabcbb")
-    c = f("abcbefgf")
+    c = f1("abcabcbb")
+    # c = f1("abcbefgf")
     print(c)
