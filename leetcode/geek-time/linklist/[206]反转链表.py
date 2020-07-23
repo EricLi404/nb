6c253451e5b13ce494fb9324da9d33f4e385baf9
LiYuyang <liyuyang@huami.com>
2020-07-23 20:58:36 +0800

    cmt

---
+++ leetcode/geek-time/linklist/[206]反转链表.py
@@ -0,0 +1,36 @@
+# 反转一个单链表。 
+# 
+#  示例: 
+# 
+#  输入: 1->2->3->4->5->NULL
+# 输出: 5->4->3->2->1->NULL 
+# 
+#  进阶: 
+# 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？ 
+#  Related Topics 链表 
+#  👍 1112 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pre = ListNode(0)
+        while head.next is not None:
+            h = head
+            h.next = pre.next
+            pre.next = h
+        return pre.next
+
+def arr2linklist(arr):
+    root = ListNode(arr.pop(0))
+    p = root
+    for i in arr:
+
+if __name__ == '__main__':
