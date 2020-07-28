f0a8cf68c7e375bde9fcf2e0dc70cafaa26ff05b
LiYuyang <liyuyang@huami.com>
2020-07-28 15:29:46 +0800

    cmt

---
+++ leetcode/geek-time/linklist/[19]删除链表的倒数第N个节点.swift
@@ -0,0 +1,38 @@
+///给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。 
+///
+/// 示例： 
+///
+/// 给定一个链表: 1->2->3->4->5, 和 n = 2.
+///
+///当删除了倒数第二个节点后，链表变为 1->2->3->5.
+/// 
+///
+/// 说明： 
+///
+/// 给定的 n 保证是有效的。 
+///
+/// 进阶： 
+///
+/// 你能尝试使用一趟扫描实现吗？ 
+/// Related Topics 链表 双指针 
+/// 👍 905 👎 0
+
+
+///leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+class Solution {
+    func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        
+    }
+}
+///leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/geek-time/linklist/[21]合并两个有序链表.swift
@@ -0,0 +1,30 @@
+///将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+///
+/// 
+///
+/// 示例： 
+///
+/// 输入：1->2->4, 1->3->4
+///输出：1->1->2->3->4->4
+/// 
+/// Related Topics 链表 
+/// 👍 1173 👎 0
+
+
+///leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        
+    }
+}
+///leetcode submit region end(Prohibit modification and deletion)
